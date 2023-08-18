7362279e186b942783556ac1cb0d9bc5b85e344c
PlatenPrime <proshta@gmail.com>
2023-08-18 15:17:06 +0300

    18.08.2023

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function About() {
+	// throw new Error("My Error")
+	return (
+		<>
+			<h1>ABOUT</h1>
+			<Link href="/">To home Page</Link>
+		</>
+	)
+}
