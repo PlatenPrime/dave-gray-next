4c3db0ae1580628c2e4490aba14d2cb49dcfd2d3
Aleksandr Marchenko <proshta@gmail.com>
2024-02-13 15:11:06 +0200

    13.02.2024

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 
 
@@ -14,18 +15,22 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
 	const searchParams = useSearchParams();
 	const pathname = usePathname();
-  const { replace } = useRouter();
+	const { replace } = useRouter();
 
 
-	function handleSearch(term: string) {
+	const handleSearch = useDebouncedCallback((term) => {
+
+		console.log(`Searching... ${term}`);
+
 		const params = new URLSearchParams(searchParams);
+		params.set('page', '1');
 		if (term) {
 			params.set('query', term);
 		} else {
 			params.delete('query');
 		}
 		replace(`${pathname}?${params.toString()}`);
-	}
+	}, 300);
 
 
 	return (
